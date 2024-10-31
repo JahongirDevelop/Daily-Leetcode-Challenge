7789f07b0f4dcddeea15170ee3cff113dfab3bac
JahongirDevelop <sjahongir221@gmail.com>
2024-10-31 11:48:25 +0500

    Problem_860

---
+++ src/main/java/leetcode/com/leetcodedailychallenge/easy/Problem_860.java
@@ -0,0 +1,12 @@
+package leetcode.com.leetcodedailychallenge.easy;
+
+public class Problem_860 {
+     /*
+      Path :
+      https://leetcode.com/problems/lemonade-change
+      */
+
+    public static void main(String[] args) {
+
+    }
+}
