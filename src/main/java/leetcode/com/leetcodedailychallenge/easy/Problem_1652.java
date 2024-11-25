3d6aff3ce847f5cd711485e0295a1358738d0426
JahongirDevelop <sjahongir221@gmail.com>
2024-11-25 17:15:14 +0500

    Problem_1652

--- src/main/java/leetcode/com/leetcodedailychallenge/easy/Problem_1652.java
+++ src/main/java/leetcode/com/leetcodedailychallenge/easy/Problem_1652.java
@@ -13,30 +13,28 @@ public class Problem_1652 {
         System.out.println(Arrays.toString(decrypt(code, k)));
     }
 
-    public static int[] decrypt(int[] code, int k) {
-        int[] result = new int[code.length];
-        if (k == 0) {
+        public static int[] decrypt(int[] code, int k) {
+            int[] result = new int[code.length];
+            if (k == 0) {
+                return result;
+            }
+
+            for (int i = 0; i < code.length; i++) {
+                int res = 0;
+                if (k > 0) {
+                    for (int j = 1; j <= k; j++) {
+                        res += code[(i + j) % code.length];
+                    }
+                } else {
+                    for (int j = 1; j <= -k; j++) {
+                        res += code[(i - j + code.length) % code.length];
+                    }
+                }
+                result[i] = res;
+            }
+
             return result;
         }
 
-        for (int i = 0; i < code.length; i++) {
-            result[i] = plus(code, i, k);
-        }
-        return result;
-    }
 
-    private static int plus(int[] code, int index, int k) {
-        int res = 0;
-        if (k > 0) {
-            for (int j = 1; j <= k; j++) {
-                res += code[(index + j) % code.length];
-            }
-        } else {
-            for (int j = 1; j <= -k; j++) {
-                res += code[(index - j + code.length) % code.length];
-            }
-        }
-
-        return res;
-    }
 }
