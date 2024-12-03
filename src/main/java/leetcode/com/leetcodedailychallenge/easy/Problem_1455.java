6035ce891cc634cc053224858716c52c02c45fcb
JahongirDevelop <sjahongir221@gmail.com>
2024-12-03 10:28:05 +0500

    Problem_1455

--- src/main/java/leetcode/com/leetcodedailychallenge/easy/Problem_1455.java
+++ src/main/java/leetcode/com/leetcodedailychallenge/easy/Problem_1455.java
@@ -1,6 +1,10 @@
 package leetcode.com.leetcodedailychallenge.easy;
 
 public class Problem_1455 {
+    /*
+     Path :
+          https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence
+     */
     public static void main(String[] args) {
         System.out.println(isPrefixOfWord("i love eating burger", "burg"));
     }
