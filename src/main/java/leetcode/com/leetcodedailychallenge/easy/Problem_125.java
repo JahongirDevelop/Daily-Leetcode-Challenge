1fea087a943bda95b03a8167454ea9b2e86fe016
JahongirDevelop <sjahongir221@gmail.com>
2025-01-07 19:21:10 +0500

    Problem_125

--- src/main/java/leetcode/com/leetcodedailychallenge/easy/Problem_125.java
+++ src/main/java/leetcode/com/leetcodedailychallenge/easy/Problem_125.java
@@ -15,31 +15,5 @@ public class Problem_125 {
         return result.contentEquals(new StringBuilder(result).reverse());
     }
 
-    public int[] decrypt(int[] code, int k) {
-        int[] result = new int[code.length];
-        if (k == 0) {
-            return result;
-        }
-
-        for (int i = 0; i < code.length; i++) {
-            result[i] = plus(code, i, k);
-        }
-        return result;
-    }
-
-    private int plus(int[] code, int index, int k) {
-        int res = 0;
-        if (k > 0) {
-            for (int j = 1; j <= k; j++) {
-                res += code[(index + j) % code.length];
-            }
-        } else {
-            for (int j = 1; j <= -k; j++) {
-                res += code[(index - j + code.length) % code.length];
-            }
-        }
-
-        return res;
-    }
 
 }
