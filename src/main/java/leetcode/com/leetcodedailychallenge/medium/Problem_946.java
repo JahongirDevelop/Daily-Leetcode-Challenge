568c0595bb8080f2e92732766ced7b3df70a88dd
JahongirDevelop <sjahongir221@gmail.com>
2025-01-13 11:56:58 +0500

    Problem_946

---
+++ src/main/java/leetcode/com/leetcodedailychallenge/medium/Problem_946.java
@@ -0,0 +1,30 @@
+package leetcode.com.leetcodedailychallenge.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem_946 {
+    public static void main(String[] args) {
+        String[] words1 = {"amazon","apple","facebook","google","leetcode"}, words2 = {"e","o"};
+        System.out.println(wordSubsets(words1, words2));
+    }
+    public static List<String> wordSubsets(String[] words1, String[] words2) {
+        List<String> result = new ArrayList<>();
+        int count = 0;
+        for (int i = 0; i < words1.length; i++) {
+            for (int j = 0; j < words2.length; j++) {
+                if (words1[i].contains(words2[j])) {
+                    count++;
+                }
+
+                if (count == words2.length) {
+                    result.add(words1[i]);
+                } else {
+                    count = 0;
+                }
+            }
+        }
+
+        return result;
+    }
+}
